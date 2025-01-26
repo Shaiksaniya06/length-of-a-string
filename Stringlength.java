b72024435976edd36e4736a282d4481b051d4588
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 13:12:21 +0530

    Add files via upload

---
+++ Stringlength.java
@@ -0,0 +1,10 @@
+public class Stringlength {
+    public static void main(String[] args) {
+        String str="hello,world!";
+        int length=str.length();
+        System.out.println("the length of string is :" + length);
+    }
+}
+
+    
+
